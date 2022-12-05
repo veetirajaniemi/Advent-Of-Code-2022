ba01ec4627178beaa1d648b6131d7cdbb2aa4e91
veetirajaniemi <veeti.rajaniemi@gmail.com>
2022-12-05 22:12:20 +0200

    Door 5 update without cheating :)

--- adventofcode5.py
+++ adventofcode5.py
@@ -1,18 +1,23 @@
 def main():
-    l1 = ["D","B","J","V"]
-    l2 = ["P","V","B","W","R","D","F"]
-    l3 = ["R","G","F","L","D","C","W","Q"]
-    l4 = ["W","J","P","M","L","N","D","B"]
-    l5 = ["H","N","B","P","C","S","Q"]
-    l6 = ["R","D","B","S","N","G"]
-    l7 = ["Z","B","P","M","Q","F","S","H"]
-    l8 = ["W","L","F"]
-    l9 = ["S","V","F","M","R"]
-    lists = [l1, l2, l3, l4, l5, l6, l7, l8, l9]
-
     input = open("input5.txt","r")
-    for i in range(0,10):
+    lists = [[] for i in range(0,9)]
+    
+    for i in range(0,8):
+        x = input.readline()
+        str = x[:-1]
+        if (len(str) == 0):
+            break
+        letters = str[1::4]
+        
+        for j in range(0,len(letters)):
+            if letters[j] != " ":
+                lists[j].append(letters[j])
+
+    for i in range(0,2):
         input.readline()
+
+    for i in lists:
+        i.reverse()
 
     while(True):
         x = input.readline()
@@ -40,5 +45,7 @@ def main():
     for i in range(0,len(lists)):
         cur = lists[i]
         print(cur[-1], end = "")
+        
+
 
 main()
